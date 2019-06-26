91556ffe6a9b0dacb64f2214eb14e4002cbf362f
Morgan Reilly <G00303598@gmit.ie>
2019-06-26 16:00:33 +0100

    Associationg Snippets with users

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,8 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -10,10 +12,23 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a snippet instance
     """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
