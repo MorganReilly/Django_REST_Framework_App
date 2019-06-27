c49565981bf896ae4f639ba06dc5fe78d9dee76f
Morgan Reilly <G00303598@gmit.ie>
2019-06-27 12:52:55 +0100

    Schema added. Fully working pastebin web api

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 from snippets import views
+
+# Including a schema for API
+schema_view = get_schema_view(title='Pastebin API')
 
 # Create a router and register our viewset with it.
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
-router.regiser(r'users', views.UserViewSet)
+router.register(r'users', views.UserViewSet)
 
 # API URLs are now determined automatically by the router
 urlpatterns = [
+    path('schema/', schema_view),
     path('', include(router.urls)),
 ]
